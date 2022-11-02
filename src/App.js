1092dc60e89044cef69e0212876d9ca11cc3b87f
hayeon <hayun4475@gmail.com>
2022-11-02 19:26:39 +0900

    feat: 랜덤한 세자리의 숫자 생성
    2022.11.02 랜덤함수를 사용하여 겹치지 않는 세자리 숫자를 생성하는 기능
    -MissionUtils 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    // 랜덤으로 겹치지 않는 셋자리 숫자 반환
+    const randomArray = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    const random = randomArray.join('');
+    MissionUtils.Console.print(random);
 }
+}
+const app = new App();
+app.play();
 
 module.exports = App;
